8b645735e0c4047fe601f93b967d26a85492f61d
jerry <s1062940@yahoo.com>
2024-11-13 18:31:05 +0800

    feat: scrollBehavior增加savedPosition,固定置頂

--- src/router/scrollBehavior.ts
+++ src/router/scrollBehavior.ts
@@ -2,8 +2,12 @@ import { searchCardScrollRecord } from '@/provides/bgScreenshot'
 import { RouterOptions } from 'vue-router'
 
 export default {
-	scrollBehavior(_, from) {
+	scrollBehavior(_, from, savedPosition) {
 		if (from.name === '/photo/[id]')
 			return { top: searchCardScrollRecord.value, behavior: 'instant' }
+
+		if (savedPosition) return savedPosition
+
+		return { top: 0, behavior: 'instant' }
 	},
 } as Partial<RouterOptions>
